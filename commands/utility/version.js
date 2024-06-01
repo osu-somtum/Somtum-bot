04c4543ead44840040ed49b394be21736c89ffa0
Phapoom Saksri <mind@blueskychan.dev>
2024-06-01 16:50:59 +0700

    0.3.0-dev
    Changed: online status

--- commands/utility/version.js
+++ commands/utility/version.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder} = require('discord.js');
-let version = "0.2.5-dev";
+let version = "0.3.0-dev";
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('botversion')
